5d7d1968d2897cd28058ba294c1f1ca365104755
Julie Burgoon <julieburgoon@Julies-Air.domain>
2021-08-16 07:46:13 -0600

    finished

---
+++ src/pokecard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import pokedex from './pokedex'
+
+const Pokecard = () =>{
+    return pokedex.map(poke => {
+     return (<div className="pokecard">
+    <h3 className="pokecard-name">{poke.name}</h3>
+    <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${poke.id}.png`} alt="pokemon"></img>
+    <h3 className="pokecard-type">{poke.type}</h3>
+    <h3 className="pokecard-exp">{poke.base_experience}</h3>
+    </div>)
+     
+    })
+    
+}
+
+export default Pokecard;
